992860d377828cbe01e642012defb12b8d689b86
Loubna Gharbi <loulizoga@gmail.com>
2024-12-06 23:52:31 +0100

    07/12

--- portfolio-loubna/src/pages/About.jsx
+++ portfolio-loubna/src/pages/About.jsx
@@ -5,20 +5,13 @@ const About = () => (
   <div className="about">
     <h1>À propos de moi</h1>
     <p>
-      Bonjour, je m'appelle Loubna, une développeuse web passionnée par la création 
-      d'applications intuitives et performantes.
-    </p>
-    <h2>Formations</h2>
-    <ul>
-      <li>Master en contrôle qualité dans les industries pharmaceutiques, alimentaires et cosmétiques.</li>
-      <li>Formation en développement web, spécialisée en JavaScript, React, et Node.js.</li>
-    </ul>
-    <h2>Compétences</h2>
-    <ul>
-      <li>Développement front-end : React, HTML, CSS, SCSS</li>
-      <li>Développement back-end : Node.js, Express, MongoDB</li>
-      <li>Gestion de version : Git/GitHub</li>
-    </ul>
+    Je suis une personne polyvalente, passionnée par le développement et toujours en quête d'excellence.
+Au fil de mes expériences, j'ai exploré divers aspects du développement web, que ce soit en création d'interfaces dynamiques avec React ou en développement d'APIs robustes avec Node.js.
+
+Mon approche se distingue par mon attention aux détails et ma capacité à gérer des projets de toutes tailles, en mettant un point d'honneur à améliorer leur qualité et leur performance. Je cherche toujours à proposer des solutions adaptées aux besoins des utilisateurs tout en respectant les délais et les exigences.
+
+Je m'efforce d'être un membre clé de chaque équipe avec laquelle je collabore, en contribuant de manière significative à la réussite des projets. Mon objectif est de produire un travail de qualité, en apportant des idées innovantes sans imposer mes choix, pour un développement harmonieux et efficace.</p>
+  
   </div>
 );
 
